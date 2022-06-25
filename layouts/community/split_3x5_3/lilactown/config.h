dbf8345539d200702df3aa26fe65e4b1f9608028
Will Acton <will@willacton.com>
2022-06-25 12:08:30 -0700

    use outer most thumb keys on skeletyl

--- layouts/community/split_3x5_3/lilactown/config.h
+++ layouts/community/split_3x5_3/lilactown/config.h
@@ -12,5 +12,5 @@ LAYOUT_split_3x5_3(                                                             
     KEY00, KEY01, KEY02, KEY03, KEY04,               KEY05, KEY06, KEY07, KEY08, KEY09, \
     KEY10, KEY11, KEY12, KEY13, KEY14,               KEY15, KEY16, KEY17, KEY18, KEY19, \
     KEY20, KEY21, KEY22, KEY23, KEY24,               KEY25, KEY26, KEY27, KEY28, KEY29, \
-                  KC_NO, KEY30, KEY31,               KEY32, KEY33, KC_NO                \
+                  KEY30, KEY31, KC_NO,               KC_NO, KEY32, KEY33             \
 )
